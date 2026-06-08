816bc6614f90c2306f4ae12f9efe708100fac9cb
Clictometecnologia <clictome2022@gmail.com>
2026-06-08 09:50:42 -0400

    Optimización páginas de detalle

--- src/app/propiedades/[slug]/page.tsx
+++ src/app/propiedades/[slug]/page.tsx
@@ -8,6 +8,8 @@ import Header from '@/components/Header';
 import PropertyCard from '@/components/PropertyCard';
 import PropertyDetailsClient from '@/components/PropertyDetailsClient';
 
+export const revalidate = 3600;
+
 interface PageProps {
   params: Promise<{ slug: string }>;
 }
